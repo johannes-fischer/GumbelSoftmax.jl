90f6eab1db9e9dbdbc210cf3a17aa1f987bcf095
Johannes Fischer <johannes.fischer@kit.edu>
2026-03-17 16:33:58 +0100

    update dimensions for rao gumbel softmax to categorical, latent, k

--- src/rao_gumbel_softmax.jl
+++ src/rao_gumbel_softmax.jl
@@ -4,30 +4,30 @@
 export sample_rao_gumbel_softmax
 
 function sample_conditional_gumbel(logits, D, k = 1)
-    # E should be size (k, n_samples, n_classes)
-    E = rand(Exponential(1), k, size(logits)...)
-    # Ei should be size (k, n_samples, 1)
-    D = reshape(D, 1, size(D)...)
-    Ei = sum(D .* E, dims = 3)
-    # Z should be size (n_samples, 1)
-    Z = sum(exp.(logits), dims = 2)
-    logits = reshape(logits, 1, size(logits)...)
-    Z = reshape(Z, 1, size(Z)...)
+    # E should be size (n_classes, n_samples, k)
+    E = rand(Exponential(1), size(logits)..., k)
+    # Ei should be size (1, n_samples, k)
+    D = reshape(D, size(D)..., 1)
+    Ei = sum(D .* E, dims = 1)
+    # Z should be size (1, n_samples)
+    Z = sum(exp.(logits), dims = 1)
+    logits = reshape(logits, size(logits)..., 1)
+    Z = reshape(Z, size(Z)..., 1)
     adjusted = D .* (-log.(Ei) .+ log.(Z)) .+ (1 .- D) .* (-log.(E ./ exp.(logits) .+ Ei ./ Z))
     return (logits .- stop_gradient(logits)) .+ stop_gradient(adjusted)
 end
 
 function sample_rao_gumbel_softmax_no_batch(logits; k = 1, tau = 1.0, I = nothing, epsilon = 1e-10)
-    probs = softmax(logits, dims = 2)
-    num_classes = size(logits, 2)
+    probs = softmax(logits)
+    num_classes = size(logits, 1)
     if I === nothing
         # note: calling value here beacuse it can interfere with StochasticAD.jl derivative of the Categorical.
-        I = [rand(Categorical(ForwardDiff.value.(probs[i, :]))) for i in axes(probs, 1)]
+        I = [rand(Categorical(ForwardDiff.value.(probs[:, i]))) for i in axes(probs, 2)]
     end
-    D = hcat(onehot.(I, Ref(1:num_classes))...)'
+    D = hcat(onehot.(I, Ref(1:num_classes))...)
     adjusted = sample_conditional_gumbel(logits, D, k)
-    surrogate = mean(softmax(adjusted ./ tau, dims = 3), dims = 1)
-    return ((surrogate-stop_gradient(surrogate))+stop_gradient(reshape(D, 1, size(D)...)))[1, :, :]
+    surrogate = mean(softmax(adjusted ./ tau), dims = 3)
+    return ((surrogate-stop_gradient(surrogate))+stop_gradient(reshape(D, size(D)..., 1)))[:, :, 1]
 end
 
 """
@@ -35,8 +35,8 @@ end
 
 Sample from the Rao-Blackwellized Gumbel-Softmax distribution. See https://arxiv.org/abs/2010.04838 for more details.
 The expected inputs are either `probs` or `logits`. If `logits` is not provided, it is computed as `log(probs + epsilon)` where `epsilon`
-is a small value to avoid numerical instability. The expected shape of `logits` is `(latent_dimension, categorial_dimension, batch_dimension)` to
-keep it consistent with Flux. `tau` is a temperature parameter that controls the smoothness of the distribution. `k` is the number of Monte Carlo samples.
+is a small value to avoid numerical instability. The expected shape of `logits` is `(categorical_dimension, batch_dimensions...)`.
+`tau` is a temperature parameter that controls the smoothness of the distribution. `k` is the number of Monte Carlo samples.
 """
 function sample_rao_gumbel_softmax(logits; k = 1, tau = 1.0, I = nothing, epsilon = 1e-10)
     return slicemap(x -> sample_rao_gumbel_softmax_no_batch(x, k = k, tau = tau, I = I, epsilon = epsilon), logits, dims = (1, 2))
--- test/rao_gumbel_softmax.jl
+++ test/rao_gumbel_softmax.jl
@@ -1,19 +1,22 @@
 using ForwardDiff
 using GumbelSoftmax
+using NNlib: softmax
 using Test
 using Zygote
 using Statistics
 
 @testset "Test conditional" begin
-    logits = [[1.0 1.5 0.8]; [0.3 0.2 0.5]]
-    D = [[1 0 0]; [0 1 0]]
+    logits = [1.0 0.3; 1.5 0.2; 0.8 0.5]
+    D = [1 0; 0 1; 0 0]
     k = 2
     n_samples = 10000
-    pytorch_truth = permutedims(cat(
-            [2.8274  1.3816  0.9552;
-                0.3226  2.0021  0.4696],
-            [2.8097  1.4182  0.9312;
-                0.3440  2.0176  0.4887], dims = 3), [3, 1, 2])
+    pytorch_truth = cat(
+            [2.8274 0.3226;
+                1.3816 2.0021;
+                0.9552 0.4696],
+            [2.8097 0.3440;
+                1.4182 2.0176;
+                0.9312 0.4887], dims = 3)
     samples = [GumbelSoftmax.sample_conditional_gumbel(logits, D, k) for i in 1:n_samples]
     mean_sample = mean(samples)
     @test mean_sample ≈ pytorch_truth rtol = 1e-1
@@ -21,9 +24,8 @@ end
 
 @testset "Test Rao Gumbel" begin
     n_samples = 10000
-    logits = [[1.0 1.5 0.8]; [0.3 0.2 0.5]]
-    logits = reshape(logits, size(logits)..., 1)
-    probs = softmax(logits, dims = 2)
+    logits = [1.0 0.3; 1.5 0.2; 0.8 0.5]
+    probs = softmax(logits)
     k = 1000
     samples = mean([sample_rao_gumbel_softmax(logits = logits, k = k) for i in 1:n_samples], dims = 1)[1]
     # we should recover categorical probs
@@ -37,14 +39,12 @@ end
         [-0.0927 0.2053 -0.1126];
         [-0.1233 -0.1126 0.2359]]
     n_grad_samples = 1000
-    logits = [[1.0 1.5 0.8];]
-    logits = reshape(logits, size(logits)..., 1)
+    logits = reshape([1.0, 1.5, 0.8], :, 1)
     grad_samples_zygote = mean([Zygote.jacobian(logits -> sample_rao_gumbel_softmax(logits = logits, k = k, tau = 0.1), logits)[1] for i in 1:n_grad_samples], dims = 1)[1]
     grad_samples_fd = mean([ForwardDiff.jacobian(logits -> sample_rao_gumbel_softmax(logits = logits, k = k, tau = 0.1), logits) for i in 1:n_grad_samples], dims = 1)[1]
     @test grad_samples_zygote ≈ pytorch_jacobian_truth_1 rtol = 1e-1
     @test grad_samples_fd ≈ pytorch_jacobian_truth_1 rtol = 1e-1
-    logits = [[0.3 0.2 0.5];]
-    logits = reshape(logits, size(logits)..., 1)
+    logits = reshape([0.3, 0.2, 0.5], :, 1)
     grad_samples_zygote = mean([Zygote.jacobian(logits -> sample_rao_gumbel_softmax(logits = logits, k = k, tau = 0.1), logits)[1] for i in 1:n_grad_samples], dims = 1)[1]
     grad_samples_fd = mean([ForwardDiff.jacobian(logits -> sample_rao_gumbel_softmax(logits = logits, k = k, tau = 0.1), logits) for i in 1:n_grad_samples], dims = 1)[1]
     @test grad_samples_zygote ≈ pytorch_jacobian_truth_2 rtol = 1e-1
